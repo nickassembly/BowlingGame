88956ed19270fab320c0bc3dd3061b5f5a00d51d
nickguerra <nickg@maliciousgames.com>
2022-04-10 10:58:38 -0500

    Added tests for full game and started working on front end

--- BowlingGame.Tests/BowlingTests.cs
+++ BowlingGame.Tests/BowlingTests.cs
@@ -1,26 +1,88 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace BowlingGame.Tests
 {
     public class BowlingTests
     {
-        Bowling game = new();
-
         [Fact]
-        public void CanStartGame()
+        public void CanRollAllZeros()
         {
-            game.StartGame();
-           
-            Assert.True(game.CurrentFrame > 0);
+            var game = SetUp();
+
+            RollForPins(game, 20, 0);
+
+            Assert.Equal(0, game.Score);
         }
 
         [Fact]
-        public void CanRollFrame()
+        public void CanRollOnePerRoll()
         {
-            game.RollFrame();
-            Assert.True(game.CurrentFrame > 1);
+            var game = SetUp();
+
+            RollForPins(game, 20, 1);
+
+            Assert.Equal(20, game.Score);
         }
 
+        [Fact]
+        public void CanRollSpare()
+        {
+            var game = SetUp();
+
+            RollSpare(game);
+            game.Roll(6);
+            RollForPins(game, 17, 0);
+
+            Assert.Equal(22, game.Score);
+        }
+
+        [Fact]
+        public void CanRollStrike()
+        {
+            var game = SetUp();
+
+            RollStrike(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollForPins(game, 16, 0);
+
+            Assert.Equal(24, game.Score);
+        }
+
+        [Fact]
+        public void CanRollAllStrikes()
+        {
+            var game = SetUp();
+
+            RollForPins(game, 21, 10);
+
+            Assert.Equal(300, game.Score);
+        }
+
+        private Bowling SetUp()
+        {
+            return new Bowling();
+        }
+
+        private void RollForPins(Bowling game, int rolls, int pinsHitPerRoll)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                game.Roll(pinsHitPerRoll);
+            }
+        }
+
+        private void RollSpare(Bowling game)
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        private void RollStrike(Bowling game)
+        {
+            game.Roll(10);
+        }
 
     }
 }
--- BowlingGame/Bowling.cs
+++ BowlingGame/Bowling.cs
@@ -2,46 +2,136 @@
 {
     public class Bowling
     {
+        private List<int> _rolls = new List<int>();
+        private int _currentRoll = 0;
+
         public Bowling()
         {
-
+            for (int i = 0; i <= 21; i++)
+            {
+                _rolls.Add(0);
+            }
         }
 
         public int CurrentFrame { get; set; } = 1;
-        public int Score { get; set; } = -1;
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                int rollIndex = 0;
+                for (int frame = 0; frame < 10; frame++)
+                {
+                    if (IsStrike(rollIndex))
+                    {
+                        score += 10 + StrikeScoreBonus(rollIndex);
+                        rollIndex++;
+                    }
+                    else if (IsSpare(rollIndex))
+                    {
+                        score += 10 + SpareScoreBonus(rollIndex);
+                        rollIndex += 2;
+                    }
+                    else
+                    {
+                        score += NormalScore(rollIndex);
+                        rollIndex += 2;
+                    }
+                }
+
+                return score;
+            }
+        }
 
         public void StartGame()
         {
-            Console.WriteLine($"Starting Game");
-            RollFrame();
+            Console.WriteLine($"Bowling Game:");
+            Console.WriteLine($"Press <enter> to start game");
+            Console.ReadLine();
+
+            for (int frame = 1; frame <= 10; frame++)
+            {
+                Console.WriteLine($"Frame: {CurrentFrame}");
+                int framePins = RollFramePins();
+
+                if (frame == 10 && framePins == 10)
+                {
+                   // Strike on 10th frame, roll 1, Player rolls 2 more times (frame 10: roll[stk] + roll roll)
+                   // Strike OR Spare on 10th frame, roll 2, player rolls 3rd time. (frame 10: roll, roll[stk or spar] + roll) 
+                }
+
+                Console.WriteLine($"Total pins in Frame {CurrentFrame} was {framePins}");
+            }
         }
 
-        public void RollFrame()
+        public int RollFramePins()
         {
-            Console.WriteLine($"Frame: {CurrentFrame}");
-            Console.WriteLine($"Press Enter to Roll for Frame {CurrentFrame}");
-            //Console.ReadLine();
+            int totalPinsInFrame = 10;
 
-            int currentRoll = 1;
+            int rollScore = CalculateRoll(totalPinsInFrame);
 
-            int maxRolls = CurrentFrame < 9 ? 2 : 3;
-
-            while (currentRoll <= maxRolls)
+            if (rollScore < totalPinsInFrame)
             {
-                int rollValue = RollBall();
-                Console.WriteLine($"Roll {currentRoll}: Pins Hit: {rollValue}");
-                currentRoll++;
-            }
+                rollScore += CalculateRoll(totalPinsInFrame - rollScore);
 
+                _rolls[_currentRoll] = rollScore;
+                CurrentFrame++;
+
+                return rollScore;
+            }
+            else
+            {
+                _rolls[_currentRoll] = rollScore;
+                CurrentFrame++;
+
+                return rollScore;
+            }
+        }
+
+        public void Roll(int pinsHit)
+        {
+            _rolls[_currentRoll++] = pinsHit;
+        }
+
+        public void ProcessStrike()
+        {
             CurrentFrame++;
         }
 
-        public int RollBall()
+        private int CalculateRoll(int pinsLeft)
         {
             Random rand = new Random();
-            int rollValue = rand.Next(0, 10);
+            int roll = rand.Next(0, pinsLeft);
 
-            return rollValue;
+            Console.WriteLine($"Roll amount: {roll}");
+            return roll;
         }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return _rolls[rollIndex] == 10;
+        }
+
+        private int StrikeScoreBonus(int rollIndex)
+        {
+            return _rolls[rollIndex + 1] + _rolls[rollIndex + 2];
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return _rolls[rollIndex] + _rolls[rollIndex + 1] == 10;
+        }
+
+        private int SpareScoreBonus(int rollIndex)
+        {
+            return _rolls[rollIndex + 2];
+        }
+
+        private int NormalScore(int rollIndex)
+        {
+            return _rolls[rollIndex] + _rolls[rollIndex + 1];
+        }
+
     }
 }
